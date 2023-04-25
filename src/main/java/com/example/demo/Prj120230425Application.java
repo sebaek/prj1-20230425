5086e49747bad1aac7290f4a3953e8e671dc7667
SeungEun Baek <sebaek@gmail.com>
2023-04-25 16:43:28 +0900

    first

---
+++ src/main/java/com/example/demo/Prj120230425Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj120230425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj120230425Application.class, args);
+	}
+
+}
