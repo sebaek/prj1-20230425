ea40b523764ab040cb6832b899d8df6efcf9e2d0
SeungEun Baek <sebaek@gmail.com>
2023-05-02 11:16:53 +0900

    transactional

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -5,12 +5,14 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
 import org.springframework.web.multipart.*;
 
 import com.example.demo.domain.*;
 import com.example.demo.mapper.*;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class BoardService {
 
 	@Autowired
@@ -27,7 +29,7 @@ public class BoardService {
 
 	public boolean modify(Board board) {
 		int cnt = mapper.update(board);
-		
+
 		return cnt == 1;
 	}
 
@@ -37,10 +39,10 @@ public class BoardService {
 	}
 
 	public boolean addBoard(Board board, MultipartFile[] files) throws Exception {
-		
+
 		// 게시물 insert
 		int cnt = mapper.insert(board);
-		
+
 		for (MultipartFile file : files) {
 			if (file.getSize() > 0) {
 				System.out.println(file.getOriginalFilename());
@@ -52,7 +54,7 @@ public class BoardService {
 				if (!targetFolder.exists()) {
 					targetFolder.mkdirs();
 				}
-				
+
 				String path = folder + "\\" + file.getOriginalFilename();
 				File target = new File(path);
 				file.transferTo(target);
@@ -60,18 +62,17 @@ public class BoardService {
 				mapper.insertFileName(board.getId(), file.getOriginalFilename());
 			}
 		}
-		
+
 		return cnt == 1;
 	}
 
-	public Map<String, Object> listBoard
-			(Integer page, String search, String type) {
+	public Map<String, Object> listBoard(Integer page, String search, String type) {
 		// 페이지당 행의 수
 		Integer rowPerPage = 15;
-		
+
 		// 쿼리 LIMIT 절에 사용할 시작 인덱스
 		Integer startIndex = (page - 1) * rowPerPage;
-		
+
 		// 페이지네이션이 필요한 정보
 		// 전체 레코드 수
 		Integer numOfRecords = mapper.countAll(search, type);
@@ -81,30 +82,22 @@ public class BoardService {
 		Integer leftPageNum = page - 5;
 		// 1보다 작을 수 없음
 		leftPageNum = Math.max(leftPageNum, 1);
-		
+
 		// 페이지네이션 오른쪽번호
 		Integer rightPageNum = leftPageNum + 9;
 		// 마지막페이지보다 클 수 없음
 		rightPageNum = Math.min(rightPageNum, lastPageNumber);
-		
+
 		Map<String, Object> pageInfo = new HashMap<>();
 		pageInfo.put("rightPageNum", rightPageNum);
 		pageInfo.put("leftPageNum", leftPageNum);
 		pageInfo.put("currentPageNum", page);
 		pageInfo.put("lastPageNum", lastPageNumber);
-		
+
 		// 게시물 목록
 		List<Board> list = mapper.selectAllPaging(startIndex, rowPerPage, search, type);
-		
-		return Map.of("pageInfo", pageInfo, 
-				      "boardList", list);
+
+		return Map.of("pageInfo", pageInfo,
+				"boardList", list);
 	}
 }
-
-
-
-
-
-
-
-
