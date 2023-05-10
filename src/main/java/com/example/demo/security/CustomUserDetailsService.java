5d58f7e7750bb039e164cf8a06b855dcb8986d6b
SeungEun Baek <sebaek@gmail.com>
2023-05-10 11:33:40 +0900

    권한얻기

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.core.authority.*;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.*;
 
@@ -17,15 +18,24 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Member member = mapper.selectById(username);
-
+		
 		if (member == null) {
 			throw new UsernameNotFoundException(username + " 회원이 없습니다.");
+		}
+		
+		//
+		
+		List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+		
+		for (String auth : member.getAuthority()) {
+			authorityList.add(new SimpleGrantedAuthority(auth));
 		}
 
 		UserDetails user = User.builder()
 				.username(member.getId())
 				.password(member.getPassword())
-				.authorities(List.of())
+				.authorities(authorityList)
+//				.authorities(member.getAuthority().stream().map(SimpleGrantedAuthority::new).toList())
 				.build();
 
 		return user;
