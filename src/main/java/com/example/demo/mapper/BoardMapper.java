9f9652d796d824ccb1b17b2fee3335dc36014493
SeungEun Baek <sebaek@gmail.com>
2023-05-16 10:24:52 +0900

    목록에서 좋아요 수

--- src/main/java/com/example/demo/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -68,7 +68,11 @@ public interface BoardMapper {
 				b.title,
 				b.writer,
 				b.inserted,
-				COUNT(f.id) fileCount
+				COUNT(f.id) fileCount,
+			    (SELECT COUNT(*) 
+			     FROM BoardLike 
+			     WHERE boardId = b.id) likeCount
+			     
 			FROM Board b LEFT JOIN FileName f ON b.id = f.boardId
 			
 			<where>
